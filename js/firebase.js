9eed42ecdcaf2be5b4eeb208f93ae5fdde1c325b
vanpham1210 <132194994+vaan021012@users.noreply.github.com>
2026-04-10 21:43:59 +0700

    Update firebase.js

--- js/firebase.js
+++ js/firebase.js
@@ -1,6 +1,11 @@
 const firebaseConfig = {
-
-  };
+  apiKey: "AIzaSyAlgW5C7WTSCzYHeqkmyz0St1giofOwMrw",
+  authDomain: "mywallet-a47af.firebaseapp.com",
+  projectId: "mywallet-a47af",
+  storageBucket: "mywallet-a47af.firebasestorage.app",
+  messagingSenderId: "169978362235",
+  appId: "1:169978362235:web:341719a8e5d3a79c15e5de"
+};
   
   firebase.initializeApp(firebaseConfig);
   const db = firebase.firestore();
